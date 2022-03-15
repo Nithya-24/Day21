4ba50180d481b885437ff1a8154fdb20637100b1
Nithya-24 <nithyaramadoz@gmail.com>
2022-03-15 12:52:47 +0530

    AnalyseMood can return Sad to pass TC Done

--- src/main/java/moodAnalyser/MoodAnalyser.java
+++ src/main/java/moodAnalyser/MoodAnalyser.java
@@ -6,19 +6,28 @@ public class MoodAnalyser {
 	public void MoodAnalyzer(String message) {
         this.message = message;
     }
+	
+	 	  
+	  public String getMessage() {
+	        return message;
+	    }
+	  
+	  public void setMessage(String message) {
+	        this.message = message;
+	    }
+	  
 	/**
 	 * convert the Message to Lowercase and check its Mood
-	 * @param message
 	 * @return
 	 */
 	
-	public String analyseMood(String message) {
+	public String analyseMood() {
 		if (message.toLowerCase().contains("happy")) {
 			return "HAPPY";
 		} else if (message.toLowerCase().contains("sad")) {
             return "SAD";
         } else if (message.toLowerCase().contains("any")) {
-            return "HAPPY";
+            return "ANY";
         } else {
             return null;
 		}
